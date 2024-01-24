0644f2f79929e7328ec015f5c7169bc523161ab0
OUTHMAN <100630135+3dqy@users.noreply.github.com>
2024-01-24 16:55:50 +0300

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,6 @@
+const darkModeToggle = document.getElementById('dark-mode');
+    const body = document.body;
+
+    darkModeToggle.addEventListener('change', () => {
+        body.classList.toggle('dark-mode');
+    });
